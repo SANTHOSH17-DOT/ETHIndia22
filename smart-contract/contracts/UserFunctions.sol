425cd47902ab2ddcf6f8e9aa26d3f4dfb7ac413f
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2022-12-04 04:34:51 +0530

    Added: check user exists function

--- smart-contract/contracts/UserFunctions.sol
+++ smart-contract/contracts/UserFunctions.sol
@@ -3,7 +3,12 @@ import "./User.sol";
 
 contract UserFunctions is User {
     event userAdded(address user);
-
+    function CheckUserExists(address user) public view returns(bool) {
+        for(uint i = 0; i < userArr.length; i++) {
+            if(userArr[i] == user) return true;
+        }
+        return false;
+    }
     function AddUser() public {
         userArr.push(msg.sender);
         emit userAdded(msg.sender);
