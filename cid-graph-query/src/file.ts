2ae2ce76aeb8b712933e049448c6dcca20e1081d
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2022-12-26 18:01:04 +0530

    Fix: file query

--- cid-graph-query/src/file.ts
+++ cid-graph-query/src/file.ts
@@ -1,7 +1,7 @@
 import {fileUploaded} from "../generated/File/File"
 import {User,File} from "../generated/schema"
 
-export function handleFileUploaded(event: fileUploaded): void {
+export function handleFileUpload(event: fileUploaded): void {
   let user = User.load(event.params.user.toHexString())
   if(user == null){
     user = new User(event.params.user.toHexString())
@@ -10,6 +10,7 @@ export function handleFileUploaded(event: fileUploaded): void {
 
   let file = new File(event.params.cid)
   file.name = event.params.name
+  file.cid = event.params.cid
   file.fileType = event.params.fileType
   file.createdAt = event.params.createdAt.toString()
   file.updateAt = event.params.updatedAt.toString()
